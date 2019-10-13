76609bf24d1c83bedbfa4eae933554041be36e2a
Tehforsch <Tonipeter92@googlemail.com>
2019-10-13 13:19:24 +0200

    initial commit

---
+++ switch.py
@@ -0,0 +1,169 @@
+import argparse
+import os
+from math import ceil
+import i3
+from config import *
+import watchContext
+
+workspaceNameFormat = "{workspaceId}:{contextName}{workspaceNumber}"
+sharedWorkspaceNameFormat = "{workspaceId}:{workspaceName}"
+
+numSharedWorkspaces = len(sharedWorkspaceNames)
+offsetPerContext = (ceil(workspacesPerContext / niceNumber)) * niceNumber
+minContextOffset = ceil(numSharedWorkspaces / offsetPerContext) * offsetPerContext
+
+SHARED_CONTEXT = -1
+
+class Context:
+    def __init__(self, id_):
+        self.id_ = id_
+
+    @property
+    def shared(self):
+        return self.id_ == SHARED_CONTEXT
+
+    @property
+    def name(self):
+        if self.shared:
+            return "Shared"
+        return contextNames[self.id_]
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.id_ == other.id_
+
+class Workspace:
+    def __init__(self, id_):
+        self.id_ = id_
+
+    @property
+    def contextNumber(self):
+        return (self.id_ - minContextOffset) // offsetPerContext
+
+    @property
+    def context(self):
+        return Context(self.contextNumber)
+
+    @property
+    def number(self):
+        return self.id_ - self.contextNumber * offsetPerContext - minContextOffset
+
+    @staticmethod
+    def fromNumbers(contextId, workspaceNumber):
+        return Workspace(minContextOffset + offsetPerContext * contextId + workspaceNumber)
+
+    @staticmethod
+    def fromFocus():
+        workspaces = i3.get_workspaces()
+        for ws in workspaces:
+            if ws['focused'] == True:
+                if ws['name'] in sharedWorkspaceNames:
+                    return Workspace.fromSharedName(ws['name'])
+                else:
+                    return Workspace(ws['num'])
+
+    @staticmethod
+    def fromSharedName(name):
+        return Workspace(sharedWorkspaceNames.index(name))
+
+    @property
+    def name(self):
+        if self.context.shared:
+            return sharedWorkspaceNames[self.id_]
+        return workspaceNameFormat.format(workspaceId=self.id_, contextName=self.context.name, workspaceNumber=self.number)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return str(self)
+
+# def getSharedWorkspaceId(sharedWorkspaceNumber):
+#     return sharedWorkspaceNames.index(sharedWorkspaceNumber)
+
+# def getSharedWorkspaceName(sharedWorkspaceName):
+#     assert sharedWorkspaceName in sharedWorkspaceNames
+#     workspaceId = getSharedWorkspaceId(sharedWorkspaceName)
+#     return sharedWorkspaceNameFormat.format(workspaceId=workspaceId, workspaceName=sharedWorkspaceName)
+
+# def getWorkspaceName(contextNumber, workspaceNumber):
+#     contextName = getContextName(contextNumber)
+#     workspaceId = getWorkspaceId(contextNumber, workspaceNumber)
+#     workspaceName = workspaceNameFormat.format(contextName=contextName, workspaceNumber=workspaceNumber, workspaceId=workspaceId)
+#     return workspaceName
+
+# def switchContext(contextNumber):
+#     focusedContextNumber, focusedWorkspaceNumber = getNumbersFromId(getFocusedWorkspaceId())
+#     # If focusedContextNumber == SHARED_CONTEXT it means we're currently on a shared context.
+#     # This means we should not change workspace here at all.
+#     if focusedContextNumber != SHARED_CONTEXT:
+#         switchWorkspace(getWorkspaceName(contextNumber, focusedWorkspaceNumber))
+
+# def moveContext(contextNumber):
+#     focusedContextNumber, focusedWorkspaceNumber = getNumbersFromId(getFocusedWorkspaceId())
+#     # If focusedContextNumber == SHARED_CONTEXT it means we're currently on a shared context.
+#     # This means we should not change workspace here at all.
+#     if focusedContextNumber != SHARED_CONTEXT:
+#         workspaceName = getWorkspaceName(contextNumber, focusedWorkspaceNumber)
+#         moveWorkspace(workspaceName)
+#         switchWorkspace(workspaceName)
+
+def maybeInt(x):
+    try:
+        return int(x)
+    except ValueError:
+        return x
+
+def setupArgs():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('type', type=str, choices=["context", "workspace"], help="Whether to change context or workspace")
+    parser.add_argument('target', type=maybeInt, help="Number of target context or workspace.")
+    parser.add_argument('--move', action='store_true', help="Take the currently focused window with you while switching to new context/workspace")
+    args = parser.parse_args()
+    return args
+
+def i3SwitchWorkspace(workspace):
+    print("Switching to {}".format(workspace.name))
+    os.system("i3-msg workspace {}".format(workspace.name))
+
+def i3MoveWindow(workspace):
+    print("Moving to {}".format(workspace.name))
+    os.system("i3-msg move workspace {}".format(workspace.name))
+
+def switchOrMove(sourceWorkspace, targetWorkspace, move):
+    if move:
+        i3MoveWindow(targetWorkspace)
+    i3SwitchWorkspace(targetWorkspace)
+
+def getTargetWorkspace(args, source, lastNonSharedContext):
+    if args.type == "workspace":
+        if type(args.target) == int: # Non-shared workspace
+            if source.context.shared: 
+                # We come from a shared context. Switch to the last context 
+                # we had before we came to a shared workspace
+                return Workspace.fromNumbers(lastNonSharedContext, args.target)
+            else: 
+                # We come from a non-shared context. Just switch via the numbers
+                return Workspace.fromNumbers(source.context.id_, args.target)
+        else: # Shared workspace
+            return Workspace.fromSharedName(args.target)
+    if args.type == "context":
+        if source.context.shared:
+            return source
+        assert type(args.target) == int
+        return Workspace.fromNumbers(args.target, source.number)
+
+def main(args):
+    focusedWorkspace = Workspace.fromFocus()
+    lastNonSharedContext = watchContext.getContext()
+    targetWorkspace = getTargetWorkspace(args, focusedWorkspace, lastNonSharedContext)
+    switchOrMove(focusedWorkspace, targetWorkspace, args.move)
+
+if __name__ == "__main__":
+    args = setupArgs()
+    main(args)
---
+++ test.py
@@ -0,0 +1,79 @@
+import unittest
+import switch
+from switch import Workspace, Context
+import config
+
+class Args:
+    def __init__(self, type_, target, move):
+        self.type = type_
+        self.target = target
+        self.move = move
+
+class TestSwitch(unittest.TestCase):
+    def nonSharedWorkspaces(self, contextId):
+        for workspaceNumber in range(1, config.workspacesPerContext+1):
+            yield Workspace.fromNumbers(contextId, workspaceNumber)
+
+    def sharedWorkspaces(self):
+        for name in config.sharedWorkspaceNames:
+            yield Workspace.fromSharedName(name)
+
+    @property
+    def contexts(self):
+        for targetNumber in range(0, len(config.contextNames)):
+            yield Context(targetNumber)
+
+    def testSwitchWorkspace(self):
+        for context in self.contexts:
+            for workspace1 in self.nonSharedWorkspaces(context.id_):
+                for workspace2 in self.nonSharedWorkspaces(context.id_):
+                    args = Args("workspace", workspace2.number, False)
+                    target = switch.getTargetWorkspace(args, workspace1, context.id_)
+                    self.assertEqual(target.number, workspace2.number)
+                    self.assertEqual(target.context, workspace1.context)
+                    self.assertEqual(target.context, context)
+
+    def testSwitchContext(self):
+        for context1 in self.contexts:
+            for workspace in self.nonSharedWorkspaces(context1.id_):
+                for context2 in self.contexts:
+                    args = Args("context", context2.id_, False)
+                    target = switch.getTargetWorkspace(args, workspace, context1.id_)
+                    self.assertEqual(target.number, workspace.number)
+                    self.assertEqual(target.context, context2)
+
+    def testSwitchNonSharedToSharedWorkspace(self):
+        for context in self.contexts:
+            for workspace1 in self.nonSharedWorkspaces(context.id_):
+                for workspace2 in self.sharedWorkspaces():
+                    args = Args("workspace", workspace2.name, False)
+                    # Switch to the shared workspace
+                    target = switch.getTargetWorkspace(args, workspace1, context.id_)
+                    self.assertEqual(target.id_, workspace2.id_)
+                    self.assertEqual(target.context.id_, switch.SHARED_CONTEXT)
+                    # Switch back. Check that the context is the previous one
+                    args = Args("workspace", workspace1.number, False)
+                    target = switch.getTargetWorkspace(args, workspace2, context.id_)
+                    self.assertEqual(target.number, workspace1.number)
+                    self.assertEqual(target.context, context)
+
+    def testSwitchSharedToShared(self):
+        for context in self.contexts:
+            for workspace1 in self.sharedWorkspaces():
+                for workspace2 in self.sharedWorkspaces():
+                    args = Args("workspace", workspace2.name, False)
+                    target = switch.getTargetWorkspace(args, workspace1, context.id_)
+                    self.assertEqual(target.name, workspace2.name)
+                    self.assertEqual(target.context.id_, switch.SHARED_CONTEXT)
+
+    def testSwitchingContextWhenOnSharedWorkspace(self):
+        for context in self.contexts:
+            for workspace in self.sharedWorkspaces():
+                args = Args("context", context.id_, False)
+                target = switch.getTargetWorkspace(args, workspace, context.id_)
+                self.assertEqual(target, workspace)
+                self.assertEqual(target.context.id_, switch.SHARED_CONTEXT)
+
+if __name__ == '__main__':
+    unittest.main()
+
